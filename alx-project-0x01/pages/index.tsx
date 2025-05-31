b67e9f2166a1fd996d3ee288cc0ddee93072800a
Rose Mwangi <muthonirosee7@gmail.com>
2025-05-31 16:51:56 +0300

    Setting up project-1

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,8 @@
+const Home: React.FC = () => {
+  return (
+    <div classname="flex justify-center items-center h-screen">
+        <h1 classname="text-7xl font-thin">Welcome Page</h1>
+    </div>
+  )
+}
+export default Home;
